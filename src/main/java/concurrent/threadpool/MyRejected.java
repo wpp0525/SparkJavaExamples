3a1e8ba438c65e50b921b32b3439acfdbee634af
wangpengpeng <1215303889@qq.com>
2019-05-10 17:06:55 +0800

    添加一些spark的算子操作

--- src/main/java/concurrent/threadpool/MyRejected.java
+++ src/main/java/concurrent/threadpool/MyRejected.java
@@ -13,7 +13,6 @@ public class MyRejected implements RejectedExecutionHandler{
 	public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
 		System.out.println("自定义处理..");
 		System.out.println("当前被拒绝任务为：" + r.toString());
-		
 
 	}
 
